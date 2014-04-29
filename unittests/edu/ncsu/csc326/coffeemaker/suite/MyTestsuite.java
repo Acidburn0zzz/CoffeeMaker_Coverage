bab6eaa16012381290c20e503acf23e2480f2d93
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-04-29 12:05:39 +0200

    Added Suite

---
+++ unittests/edu/ncsu/csc326/coffeemaker/suite/MyTestsuite.java
@@ -0,0 +1,18 @@
+package edu.ncsu.csc326.coffeemaker.suite;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import edu.ncsu.csc326.coffeemaker.AddRecipeTest;
+import edu.ncsu.csc326.coffeemaker.CheckInventoryTest;
+import edu.ncsu.csc326.coffeemaker.CoffeeMakerTest;
+import edu.ncsu.csc326.coffeemaker.DeleteRecipeTest;
+import edu.ncsu.csc326.coffeemaker.EditRecipeTest;
+import edu.ncsu.csc326.coffeemaker.PurchaseCoffeeTest;
+
+public class MyTestsuite extends TestSuite {
+	public static Test suite() {
+		return new TestSuite(CoffeeMakerTest.class, AddRecipeTest.class,
+				CheckInventoryTest.class, DeleteRecipeTest.class,
+				EditRecipeTest.class, PurchaseCoffeeTest.class);
+	}
+}
