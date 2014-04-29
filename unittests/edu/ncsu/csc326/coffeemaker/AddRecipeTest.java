9f3384abbd7f70a21d5e5b3e9bd9e57f62c1fbbc
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-04-29 11:48:40 +0200

    ...

--- unittests/edu/ncsu/csc326/coffeemaker/AddRecipeTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/AddRecipeTest.java
@@ -2,22 +2,20 @@ package edu.ncsu.csc326.coffeemaker;
 
 import junit.framework.Assert;
 
-public class AddRecipeTest extends SetupTest{
-	
+public class AddRecipeTest extends SetupTest {
+
 	public void addMoreThan3Recipes() {
 		Assert.assertTrue(cm.addRecipe(r1));
 		Assert.assertTrue(cm.addRecipe(r2));
 		Assert.assertTrue(cm.addRecipe(r3));
-		Assert.assertFalse(cm.addRecipe(r4));
+		Assert.assertFalse(cm.addRecipe(r4));mnh
 	}
 
-	
 	public void testAddRecipeTwice() {
 		Assert.assertTrue(cm.addRecipe(r1));
 		Assert.assertFalse(cm.addRecipe(r1));
 	}
 
-	
 	public void testAddIdenticalRecipe() {
 		Assert.assertTrue(cm.addRecipe(r1));
 
@@ -32,35 +30,28 @@ public class AddRecipeTest extends SetupTest{
 		Assert.assertFalse(cm.addRecipe(r1));
 	}
 
-	
 	public void testAddInconsistentRecipe() {
 		Recipe recipe = new Recipe();
 		recipe.setName("Test");
 
 		Assert.assertFalse(cm.addRecipe(recipe));
 	}
-	
 
 	public void testSetPrice() {
 		r1.setPrice(-3);
 		Assert.assertEquals(0, r1.getPrice());
 	}
 
-
-
 	public void testAddRecipeWithNegativeAmount1() {
 		r1.setAmtChocolate(-7);
 		Assert.assertEquals(0, r1.getAmtChocolate());
 	}
 
-	
-	
-	public void testToString(){
+	public void testToString() {
 		Assert.assertNotNull(r1.toString());
 	}
-	
-	
-	public void testHashcode(){
+
+	public void testHashcode() {
 		Assert.assertNotNull(r1.hashCode());
 	}
 
