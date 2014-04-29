fceca33831e879936dd5029b53cfd6a5fad414d4
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-04-29 10:43:34 +0200

    ..

--- unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -1,21 +1,38 @@
 package edu.ncsu.csc326.coffeemaker;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
-import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
+import junit.framework.Assert;
 
 /**
  * 
  * @author Sarah Heckman
- *
- * Unit tests for CoffeeMaker class.
+ * 
+ *         Unit tests for CoffeeMaker class.
  */
 public class CoffeeMakerTest {
-	
+
+	// Beispiel:
+	//
+	// private CoffeeMaker cm;
+	// private Inventory i;
+	// private Recipe r1;
+	//
+	// public void setUp() {
+	// cm = new CoffeeMaker();
+	// i = cm.checkInventory();
+	//
+	// r1 = new Recipe();
+	// r1.setName("Coffee");
+	// r1.setPrice(50);
+	// r1.setAmtCoffee(6);
+	// r1.setAmtMilk(1);
+	// r1.setAmtSugar(1);
+	// r1.setAmtChocolate(0);
+	// }
+	//
+	// public void testAddRecipe() {
+	// assertTrue(cm.addRecipe(r1));
+	// }
+
 	private CoffeeMaker cm;
 	private Recipe r1;
 	private Recipe r2;
@@ -25,8 +42,8 @@ public class CoffeeMakerTest {
 	@Before
 	public void setUp() throws Exception {
 		cm = new CoffeeMaker();
-		
-		//Set up for r1
+
+		// Set up for r1
 		r1 = new Recipe();
 		r1.setName("Coffee");
 		r1.setAmtChocolate("0");
@@ -34,8 +51,8 @@ public class CoffeeMakerTest {
 		r1.setAmtMilk("1");
 		r1.setAmtSugar("1");
 		r1.setPrice("50");
-		
-		//Set up for r2
+
+		// Set up for r2
 		r2 = new Recipe();
 		r2.setName("Mocha");
 		r2.setAmtChocolate("20");
@@ -43,8 +60,8 @@ public class CoffeeMakerTest {
 		r2.setAmtMilk("1");
 		r2.setAmtSugar("1");
 		r2.setPrice("75");
-		
-		//Set up for r3
+
+		// Set up for r3
 		r3 = new Recipe();
 		r3.setName("Latte");
 		r3.setAmtChocolate("0");
@@ -52,8 +69,8 @@ public class CoffeeMakerTest {
 		r3.setAmtMilk("3");
 		r3.setAmtSugar("1");
 		r3.setPrice("100");
-		
-		//Set up for r4
+
+		// Set up for r4
 		r4 = new Recipe();
 		r4.setName("Hot Chocolate");
 		r4.setAmtChocolate("4");
@@ -62,27 +79,26 @@ public class CoffeeMakerTest {
 		r4.setAmtSugar("1");
 		r4.setPrice("65");
 	}
-	
+
 	@Test
 	public void testMakeCoffee() {
 		cm.addRecipe(r1);
 		Assert.assertEquals(25, cm.makeCoffee(0, 75));
 	}
-	
+
 	@Test
 	public void testAddInventory() throws InventoryException {
-			cm.addInventory("4","7","0","9");
+		cm.addInventory("4", "7", "0", "9");
 	}
-	
+
 	@Test
-	public void testCHeckInventory(){
+	public void testCHeckInventory() {
 		Assert.assertNotNull(cm.checkInventory());
 	}
 
 	@Test(expected = InventoryException.class)
 	public void testAddInventoryException() throws InventoryException {
-			cm.addInventory("4", "-1", "asdf", "3");
+		cm.addInventory("4", "-1", "asdf", "3");
 	}
-	
-	
+
 }
