463d92cc5dd08cf3c14df6ffa101ed36be138b76
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-04-29 10:48:11 +0200

    ...

--- unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -22,7 +22,7 @@ public class CoffeeMakerTest extends TestCase {
 	// i = cm.checkInventory();
 	//
 	// r1 = new Recipe();
-	// r1.setName("Coffee");
+	// r1.setName(Coffee);
 	// r1.setPrice(50);
 	// r1.setAmtCoffee(6);
 	// r1.setAmtMilk(1);
@@ -40,66 +40,60 @@ public class CoffeeMakerTest extends TestCase {
 	private Recipe r3;
 	private Recipe r4;
 
-	@Before
 	public void setUp() throws Exception {
 		cm = new CoffeeMaker();
 
 		// Set up for r1
 		r1 = new Recipe();
 		r1.setName("Coffee");
-		r1.setAmtChocolate("0");
-		r1.setAmtCoffee("3");
-		r1.setAmtMilk("1");
-		r1.setAmtSugar("1");
-		r1.setPrice("50");
+		r1.setAmtChocolate(0);
+		r1.setAmtCoffee(3);
+		r1.setAmtMilk(1);
+		r1.setAmtSugar(1);
+		r1.setPrice(50);
 
 		// Set up for r2
 		r2 = new Recipe();
 		r2.setName("Mocha");
-		r2.setAmtChocolate("20");
-		r2.setAmtCoffee("3");
-		r2.setAmtMilk("1");
-		r2.setAmtSugar("1");
-		r2.setPrice("75");
+		r2.setAmtChocolate(20);
+		r2.setAmtCoffee(3);
+		r2.setAmtMilk(1);
+		r2.setAmtSugar(1);
+		r2.setPrice(75);
 
 		// Set up for r3
 		r3 = new Recipe();
 		r3.setName("Latte");
-		r3.setAmtChocolate("0");
-		r3.setAmtCoffee("3");
-		r3.setAmtMilk("3");
-		r3.setAmtSugar("1");
-		r3.setPrice("100");
+		r3.setAmtChocolate(0);
+		r3.setAmtCoffee(3);
+		r3.setAmtMilk(3);
+		r3.setAmtSugar(1);
+		r3.setPrice(100);
 
 		// Set up for r4
 		r4 = new Recipe();
 		r4.setName("Hot Chocolate");
-		r4.setAmtChocolate("4");
-		r4.setAmtCoffee("0");
-		r4.setAmtMilk("1");
-		r4.setAmtSugar("1");
-		r4.setPrice("65");
+		r4.setAmtChocolate(4);
+		r4.setAmtCoffee(0);
+		r4.setAmtMilk(1);
+		r4.setAmtSugar(1);
+		r4.setPrice(65);
 	}
 
-	@Test
 	public void testMakeCoffee() {
-		cm.addRecipe(r1);
-		Assert.assertEquals(25, cm.makeCoffee(0, 75));
+		Assert.assertEquals(25, cm.makeCoffee(r1, 75));
 	}
 
-	@Test
-	public void testAddInventory() throws InventoryException {
-		cm.addInventory("4", "7", "0", "9");
+	public void testAddInventory() {
+		cm.addInventory(4, 7, 0, 9);
 	}
 
-	@Test
 	public void testCHeckInventory() {
 		Assert.assertNotNull(cm.checkInventory());
 	}
 
-	@Test(expected = InventoryException.class)
-	public void testAddInventoryException() throws InventoryException {
-		cm.addInventory("4", "-1", "asdf", "3");
+	public void testAddInventoryException() {
+		cm.addInventory(4, -1, 3, 3);
 	}
 
 }
