6985cc01d2740f20ae7669fa1fcbd50424972583
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-04-29 11:10:16 +0200

    ...

--- unittests/edu/ncsu/csc326/coffeemaker/PurchaseCoffeeTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/PurchaseCoffeeTest.java
@@ -1,98 +1,44 @@
 package edu.ncsu.csc326.coffeemaker;
 
-import org.hamcrest.core.Is;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.Assert;
 
-public class PurchaseCoffeeTest {
-	private CoffeeMaker cm;
-	private Recipe r1;
-	private Recipe r2;
-	private Recipe r3;
-	private Recipe r4;
+public class PurchaseCoffeeTest extends SetupTest {
 
-	@Before
-	public void setUp() throws Exception {
-		cm = new CoffeeMaker();
-
-		// Set up for r1
-		r1 = new Recipe();
-		r1.setName("Coffee");
-		r1.setAmtChocolate("0");
-		r1.setAmtCoffee("3");
-		r1.setAmtMilk("1");
-		r1.setAmtSugar("1");
-		r1.setPrice("50");
-
-		// Set up for r2
-		r2 = new Recipe();
-		r2.setName("Mocha");
-		r2.setAmtChocolate("20");
-		r2.setAmtCoffee("3");
-		r2.setAmtMilk("1");
-		r2.setAmtSugar("1");
-		r2.setPrice("75");
-
-		// Set up for r3
-		r3 = new Recipe();
-		r3.setName("Latte");
-		r3.setAmtChocolate("0");
-		r3.setAmtCoffee("3");
-		r3.setAmtMilk("3");
-		r3.setAmtSugar("1");
-		r3.setPrice("100");
-
-		// Set up for r4
-		r4 = new Recipe();
-		r4.setName("Hot Chocolate");
-		r4.setAmtChocolate("4");
-		r4.setAmtCoffee("0");
-		r4.setAmtMilk("1");
-		r4.setAmtSugar("1");
-		r4.setPrice("65");
-	}
-
-	@Test
 	// Insufficient funds to purchase.
 	public void testPurchase1InsufficientFunds() {
 		cm.addRecipe(r1);
 		cm.addRecipe(r2);
 		cm.addRecipe(r3);
 
-		Assert.assertThat(cm.makeCoffee(0, 10), Is.is(10));
+		Assert.assertEquals(10, cm.makeCoffee(r1, 10));
 	}
 
-	@Test
 	public void testPurchase2() {
 		cm.addRecipe(r1);
 		cm.addRecipe(r2);
 		cm.addRecipe(r3);
 
-		Assert.assertThat(cm.makeCoffee(0, 50), Is.is(0));
+		Assert.assertEquals(0, cm.makeCoffee(r1, 50));
 	}
 
-	@Test
 	public void testPurchase3() {
 		cm.addRecipe(r1);
 		cm.addRecipe(r2);
 		cm.addRecipe(r3);
 
-		Assert.assertThat(cm.makeCoffee(0, 60), Is.is(10));
+		Assert.assertEquals(10, cm.makeCoffee(r1, 60));
 	}
 
-	@Test
 	public void testPurchase4() {
 		cm.addRecipe(r1);
 		cm.addRecipe(r2);
 		cm.addRecipe(r3);
 
-		Assert.assertThat(cm.makeCoffee(0, -60), Is.is(-60));
+		Assert.assertEquals(-60, cm.makeCoffee(r1, -60));
 	}
 
-	@Test
 	public void testPurchase5() {
-		Assert.assertThat(cm.makeCoffee(0, 60), Is.is(60));
+		Assert.assertEquals(60, cm.makeCoffee(r1, 60));
 	}
 
 }
