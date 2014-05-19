b391c2cd819522e1d14eb731bb74292b241a28a3
serious6 (andre.behrens@haw-hamburg.de) <andre.behrens@haw-hamburg.de>
2014-05-19 20:25:41 +0200

    ...

--- unittests/edu/ncsu/csc326/coffeemaker/tests/TheCoffeeMakerTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/tests/TheCoffeeMakerTest.java
@@ -56,29 +56,29 @@ public class TheCoffeeMakerTest extends SetupTest {
 	public void testAddInventoryException() {
 		cm.addInventory(4, -1, 3, 3);
 	}
-	
-	public void testCoffeMakerPush(){
+
+	public void testCoffeMakerPush() {
 		r1.setName("Zauberzutat");
 		cm.addRecipe(r1);
 		cm.addRecipe(r2);
 		cm.addRecipe(r3);
-		
+
 		Assert.assertNotNull(cm.getRecipeForName("bliBlaBlubb"));
 		Assert.assertNotNull(cm.getRecipeForName("Zauberzutat"));
-		
+
 	}
-	
-	public void testCoffeMakerPush2(){
+
+	public void testCoffeMakerPush2() {
 		cm = new CoffeeMaker();
 		Assert.assertTrue(cm.addInventory(100, 100, 100, 100));
-		
+
 		Assert.assertTrue(cm.checkInventory().getChocolate() > 100);
 		Assert.assertTrue(cm.checkInventory().getCoffee() > 100);
 		Assert.assertTrue(cm.checkInventory().getMilk() > 100);
 		Assert.assertTrue(cm.checkInventory().getSugar() > 100);
 	}
-	
-	public void testCoffeMakerPush3(){
+
+	public void testCoffeMakerPush3() {
 		cm.addInventory(85, 85, 85, 85);
 		System.out.println(cm.checkInventory().getChocolate());
 		r1.setPrice(10);
@@ -87,16 +87,16 @@ public class TheCoffeeMakerTest extends SetupTest {
 		r1.setAmtCoffee(10);
 		r1.setAmtMilk(50);
 		r1.setAmtChocolate(50);
-		
+
 		Assert.assertEquals(10, cm.makeCoffee(r1, 20));
-		
+
 		Assert.assertEquals(50, cm.checkInventory().getChocolate());
 		Assert.assertEquals(50, cm.checkInventory().getMilk());
 		Assert.assertEquals(50, cm.checkInventory().getSugar());
 		Assert.assertEquals(110, cm.checkInventory().getCoffee());
 	}
-	
-	public void testCoffeMakerPush4(){
+
+	public void testCoffeMakerPush4() {
 		Assert.assertTrue(cm.addRecipe(r1));
 		Assert.assertTrue(cm.addRecipe(r2));
 		Assert.assertTrue(cm.addRecipe(r3));
@@ -107,20 +107,29 @@ public class TheCoffeeMakerTest extends SetupTest {
 		Assert.assertFalse(cm.addRecipe(r4));
 		Recipe[] recipes = cm.getRecipes();
 		Assert.assertTrue(recipes.length > 0);
-		
+
 		String nameAtIndex0 = recipes[0].getName();
-		Assert.assertEquals(nameAtIndex0, cm.getRecipeForName(nameAtIndex0).getName());
-		
+		Assert.assertEquals(nameAtIndex0, cm.getRecipeForName(nameAtIndex0)
+				.getName());
+
 		// delete it
 		Assert.assertTrue(cm.deleteRecipe(r4));
 	}
-	
-	public void testCoffeMakerPush5(){
+
+	public void testCoffeMakerPush5() {
 		Assert.assertFalse(cm.addInventory(-85, -85, -85, -85));
 	}
-	
-	public void testCoffeMakerPush6(){
+
+	public void testCoffeMakerPush6() {
 		Assert.assertEquals(4, cm.getRecipes().length);
+
+		Recipe[] recipes = cm.getRecipes();
+		try {
+			Recipe recipe = recipes[4];
+			throw new IllegalArgumentException();
+		} catch (Exception e) {
+			Assert.assertTrue(e instanceof ArrayIndexOutOfBoundsException);
+		}
 	}
 
 }
