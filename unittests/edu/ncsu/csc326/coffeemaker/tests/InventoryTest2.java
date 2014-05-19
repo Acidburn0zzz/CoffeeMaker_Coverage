690540a16f11f102a122fe482a24c98c308e4da8
serious6 (andre.behrens@haw-hamburg.de) <andre.behrens@haw-hamburg.de>
2014-05-19 19:46:07 +0200

    Pushed Inventory to 96%

--- unittests/edu/ncsu/csc326/coffeemaker/tests/InventoryTest2.java
+++ unittests/edu/ncsu/csc326/coffeemaker/tests/InventoryTest2.java
@@ -13,5 +13,56 @@ public class InventoryTest2 extends SetupTest {
 		Assert.assertTrue(inventory.toString().contains("Sugar"));
 		Assert.assertTrue(inventory.toString().contains("Chocolate"));
 		Assert.assertTrue(inventory.toString() instanceof String);
+		
+		Assert.assertTrue(inventory.toString().length() > 1);
+	}
+	
+	public void testPush2() {
+		Inventory inventory = new Inventory();
+		
+		r1.setAmtChocolate(5);
+		r1.setAmtCoffee(5);
+		r1.setAmtMilk(5);
+		r1.setAmtSugar(5);
+		Assert.assertTrue(inventory.enoughIngredients(r1));
+		
+		r1.setAmtChocolate(5);
+		r1.setAmtCoffee(5);
+		r1.setAmtMilk(5);
+		r1.setAmtSugar(5);
+		
+		r1.setAmtCoffee(50);
+		Assert.assertFalse(inventory.enoughIngredients(r1));
+		
+		r1.setAmtChocolate(5);
+		r1.setAmtCoffee(5);
+		r1.setAmtMilk(5);
+		r1.setAmtSugar(5);
+		
+		r1.setAmtChocolate(50);
+		Assert.assertFalse(inventory.enoughIngredients(r1));
+		
+		r1.setAmtChocolate(5);
+		r1.setAmtCoffee(5);
+		r1.setAmtMilk(5);
+		r1.setAmtSugar(5);
+		
+		r1.setAmtMilk(50);
+		Assert.assertFalse(inventory.enoughIngredients(r1));
+		
+		r1.setAmtChocolate(5);
+		r1.setAmtCoffee(5);
+		r1.setAmtMilk(5);
+		r1.setAmtSugar(5);
+		
+		r1.setAmtSugar(50);
+		Assert.assertFalse(inventory.enoughIngredients(r1));
+		
+		r1.setAmtChocolate(50);
+		r1.setAmtCoffee(50);
+		r1.setAmtMilk(50);
+		r1.setAmtSugar(50);
+		
+		Assert.assertFalse(inventory.enoughIngredients(r1));
 	}
 }
